a9cd90b451971842224cdcb22f5abee88310a45b
Jesica Nikolovska <jesica.nikolovska@student.uts.edu.au>
2023-02-25 16:01:54 +1100

    Rocket Script and collision boxes

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public Rigidbody2D rocketRigidBody;
+    private Vector2 rocketMovement;
+    public float rocketSpeed;
+    
+    bool playermoving;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rocketRigidBody = GetComponent<Rigidbody2D>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        rocketMovement.y = Input.GetAxis("Vertical") * rocketSpeed;
+        rocketMovement.x = Input.GetAxis("Horizontal") * rocketSpeed;
         
+
+
     }
-}
+    private void FixedUpdate()
+    {
+
+          transform.position = transform.position + new Vector3(rocketMovement.x * Time.deltaTime, rocketMovement.y * Time.deltaTime,0);
+     
+    }
+    
+}  
